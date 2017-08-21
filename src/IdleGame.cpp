f422a2b0bf619b727a2c2dbbc17a5dc800efd2bb
EmergencyTemporalShift <jrisenhour@gmail.com>
2017-08-21 02:54:16 -0400

    Added broken signal handling, changed grammar

--- src/IdleGame.cpp
+++ src/IdleGame.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <chrono>
 #include <thread>
+#include <signal.h>
 
 
 #include "PointGenerator.h"
@@ -22,12 +23,21 @@ std::chrono::high_resolution_clock Clock;
 
 // This apparently assigns the right type to these variables
 //std::chrono::milliseconds newTime;
-auto newTime =  (std::chrono::high_resolution_clock::now());
-auto oldTime = newTime;
+auto newTime   =  (std::chrono::high_resolution_clock::now());
+auto oldTime   = newTime;
 auto deltaTime = newTime-oldTime;
+
+// For Interrupts (Doesn't work)
+volatile sig_atomic_t flag = 0;
+void sigint(int sig){ // can be called asynchronously
+	flag = 1; // set flag
+}
 
 
 int main(int argc, char** argv) {
+	// This causes a segfault in bash and does nothing in eclipse
+	//signal(SIGINT, sigint);
+
 	// Define the first generator
 	PointGenerator p(16, 1, 0);
 
@@ -36,11 +46,11 @@ int main(int argc, char** argv) {
 	 * would be as fast as the computer could trigger it
 	 */
 	p.Add();
-	// Infinite loop. Turns out you can just skip the condition in a for loop
+	// Infinite loop. Turns out you can just skip the test in a for loop
 	for (int i; ;i++) {
 		// Figure out deltaTime
-		oldTime = newTime;
-		newTime =  (std::chrono::high_resolution_clock::now());
+		oldTime   = newTime;
+		newTime   = std::chrono::high_resolution_clock::now();
 		deltaTime = newTime - oldTime;
 
 		/*
@@ -50,27 +60,36 @@ int main(int argc, char** argv) {
 		 */
 		if(i%10==0) {
 			printf("New Points: %f, ", p.GeneratePoints( ));
-			/* %f shows 0.000000, but %g shows really small numbers like 1.97626e-323
+			/*
+			 * %f shows 0.000000, but %g shows really small numbers like 1.97626e-323
 			 * Does this have to do with the way doubles are stored?
 			 * TODO: Find a way to display globalPoints properly
 			 */
 			printf("Total Points: %g\r\n", globalPoints);
-		}
 
-		/*
-		 * Buys as many PointGenerators as it can and prints how many it bought
-		 * (It's broken into two lines do to weird run order.)
-		 */
-		if(i%100==0) {
-			printf("Bought %i PointGenerators for a total of ", p.BuyMax( ));
-			printf("%i\r\n", p.getAmount());
+			/*
+			 * Buy as many PointGenerators as it can and prints how many it bought
+			 * (It's broken into two lines do to weird run order.)
+			 */
+			if(i%100==0) {
+				printf("Bought %i PointGenerators for a total of ", p.BuyMax( ));
+				printf("%i\r\n", p.getAmount());
+			}
 		}
-
+		if(flag) {
+			printf("Exiting loop");
+			break;
+		}
 		//TODO: Find out how to take an input while spamming the console with junk.
 
 		// Sleep for a tenth of a seconds
 		std::this_thread::sleep_for(std::chrono::milliseconds (100));
 		//printf("%li\n", deltaTime); // TODO: Find out how to print durations
+	}
+	// Broken out of the loop, finish up.
+	if(flag) {
+		printf("Control C pressed");
+		return 1;
 	}
 	return 0;
 }
